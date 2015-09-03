8efce6f7c81456188f5b4e8f527ce2d09a2b1871
EmanuelDubor <emanueldubora@hotmail.com>
2015-09-03 02:21:06 -0300

    Changed bullet to box2d

--- core/src/ar/edu/unq/games/arkanoid/ArkanoidGame.scala
+++ core/src/ar/edu/unq/games/arkanoid/ArkanoidGame.scala
@@ -2,6 +2,11 @@ package ar.edu.unq.games.arkanoid
 
 import ar.edu.unq.games.arkanoid.layers.{PhysicsLayer, UILayer, BackgroundLayer, Layer}
 
-class ArkanoidGame extends Layer with UILayer with PhysicsLayer with BackgroundLayer {
+class ArkanoidGame extends Layer
+      with UILayer
+      with PhysicsLayer
+      with BackgroundLayer {
+
+
 
 }
--- core/src/ar/edu/unq/games/arkanoid/layers/PhysicsLayer.scala
+++ core/src/ar/edu/unq/games/arkanoid/layers/PhysicsLayer.scala
@@ -1,5 +1,15 @@
 package ar.edu.unq.games.arkanoid.layers
 
-trait PhysicsLayer extends Layer {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
+trait PhysicsLayer extends Layer {
+//  var world:World
+
+  override def init(): Unit = {
+    super.init()
+  }
+
+  override def act(delta: Float): Unit = super.act(delta)
+
+  override def draw(spriteBatch: SpriteBatch): Unit = super.draw(spriteBatch)
 }
