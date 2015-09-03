6a08bfa85d9e68c6093cd9275dd8428958b834f2
EmanuelDubor <emanueldubora@hotmail.com>
2015-09-03 02:53:54 -0300

    Added actors.

---
+++ core/src/ar/edu/unq/games/arkanoid/actors/Ball.scala
@@ -0,0 +1,12 @@
+package ar.edu.unq.games.arkanoid.actors
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+
+class Ball extends Actor {
+  override val pos: Vector2 = _
+
+  override def draw(spriteBatch: SpriteBatch): Unit = ???
+
+  override def act(delta: Float): Unit = ???
+}
--- core/src/ar/edu/unq/games/arkanoid/layers/PhysicsLayer.scala
+++ core/src/ar/edu/unq/games/arkanoid/layers/PhysicsLayer.scala
@@ -1,15 +1,28 @@
 package ar.edu.unq.games.arkanoid.layers
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.World
 
 trait PhysicsLayer extends Layer {
-//  var world:World
+  var world: World = _
 
   override def init(): Unit = {
+    world = new World(new Vector2(0f, 0f), true)
     super.init()
   }
 
-  override def act(delta: Float): Unit = super.act(delta)
+  override def act(delta: Float): Unit = {
+    world.step(delta, 8, 3)
+    super.act(delta)
+  }
 
-  override def draw(spriteBatch: SpriteBatch): Unit = super.draw(spriteBatch)
+  override def draw(spriteBatch: SpriteBatch): Unit = {
+    super.draw(spriteBatch)
+  }
+
+  override def dispose(): Unit = {
+    world.dispose()
+    super.dispose()
+  }
 }
