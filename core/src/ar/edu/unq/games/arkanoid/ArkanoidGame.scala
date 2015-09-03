eecbb2ed2c27a340f901b9c27039c868fb52d9a9
EmanuelDubor <emanueldubora@hotmail.com>
2015-09-03 01:03:33 -0300

    Refactored ArkanoidGame and MainController

--- core/src/ar/edu/unq/games/arkanoid/ArkanoidGame.scala
+++ core/src/ar/edu/unq/games/arkanoid/ArkanoidGame.scala
@@ -1,18 +1,7 @@
 package ar.edu.unq.games.arkanoid
 
 import ar.edu.unq.games.arkanoid.layers.{BackgroundLayer, Layer}
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class ArkanoidGame extends Layer with BackgroundLayer {
-
-  override def draw(spriteBatch: SpriteBatch): Unit = {
-    Gdx.gl.glClearColor(0, 0, 0, 1)
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-    spriteBatch.begin()
-    super.draw(spriteBatch)
-    spriteBatch.end()
-  }
 
 }
--- core/src/ar/edu/unq/games/arkanoid/controllers/MainController.scala
+++ core/src/ar/edu/unq/games/arkanoid/controllers/MainController.scala
@@ -1,7 +1,8 @@
 package ar.edu.unq.games.arkanoid.controllers
 
 import ar.edu.unq.games.arkanoid.ArkanoidGame
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.{Gdx, ApplicationAdapter}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class MainController extends ApplicationAdapter {
@@ -13,6 +14,12 @@ class MainController extends ApplicationAdapter {
     arkanoidGame = new ArkanoidGame
   }
 
-  override def render(): Unit = arkanoidGame draw spriteBatch
+  override def render(): Unit = {
+    Gdx.gl.glClearColor(0, 0, 0, 1)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    spriteBatch.begin()
+    arkanoidGame.draw(spriteBatch)
+    spriteBatch.end()
+  }
 
 }
